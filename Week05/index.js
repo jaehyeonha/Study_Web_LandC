5707713839082b15a2108659ff182177c9868e0f
jaehyeonha <mini0950@knu.ac.kr>
2023-08-13 21:15:16 +0900

    js todolist

--- Week05/index.js
+++ Week05/index.js
@@ -10,38 +10,51 @@ function whenIncomingRequest(req, res) {
         res.write(data);
         return res.end();
     } 
-    
+
     if(req.url === '/users' && req.method === 'GET') {
-        const data = fs.readFileSync('./users.txt');
-        res.write(JSON.stringify(data.toString().split('\n')));
+        const data = fs.readFileSync('./users.txt').toString().trim().split('\n');
+        if(data == '') return res.end();
+        res.write(JSON.stringify(data));
         return res.end();
     }
 
     if(req.url === '/users' && req.method === 'POST') {
         let body = '';
-        req.on('data', (chunk) => {
-            body += chunk;
+        req.on('data', chunk => {
+            body += JSON.parse(chunk).name.toString();
         });
         req.on('end', () => {
-            const name = JSON.parse(body).name;
-            fs.appendFileSync('./users.txt', `${name}\n`);
-            return res.end();
+            if(body.trim() === ''){
+                res.statusCode = 400;
+                res.end('empty body');
+                return;
+            }
+            const appendData = body.trim();
+            const prevData = fs.readFileSync("./users.txt").toString().split('\n');
+            if(prevData.includes(body.trim())){
+                res.statusCode = 409;
+                res.end('already exists');
+                return;
+            }
+            fs.writeFileSync("./users.txt", prevData.join('\n') + appendData + '\n');
+            res.statusCode = 201;
+            res.end();
         });
     }
 
     if(req.url === '/users' && req.method === 'DELETE') {
         let body = '';
         req.on('data', chunk => {
-            body += chunk.toString();
+            body += JSON.parse(chunk).name.toString();
         });
         req.on('end', () => {
-            const prevData = fs.readFileSync("./users.txt").toString().trim().split('\n');
-            if(!prevData.includes("x|"+body.trim())&&!prevData.includes("o|"+body.trim())){
+            const prevData = fs.readFileSync("./users.txt").toString().split('\n');
+            if(!prevData.includes(body.trim())){
                 res.statusCode = 404;
                 res.end('not found');
                 return;
             }
-            fs.writeFileSync("./users.txt", prevData.filter(item => item.split('|')[1] !== body.trim()).join('\n'));
+            fs.writeFileSync("./users.txt", prevData.filter(item => item !== body.trim()).join('\n'));
             res.statusCode = 200;
             res.end();
         });
